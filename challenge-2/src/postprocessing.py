86a928346efcd868baea4a883ee4358aba9f1e58
Amit Lakhera <amit.lakhera@gmail.com>
2025-05-25 22:11:59 +0530

    Soil Image Single-Class Binary Classification

--- challenge-2/src/postprocessing.py
+++ challenge-2/src/postprocessing.py
@@ -1,2 +1,14 @@
 # src/postprocessing.py
 
+# This module contains the postprocessing steps for the soil image dataset predictions.
+import pandas as pd
+
+# Function to decode predictions from model output
+def decode_predictions(predictions, label_map):
+    return [label_map[pred] for pred in predictions]
+
+# Function to save the decoded predictions to a CSV file for submission
+def save_submission(ids, decoded_preds, output_csv="submission.csv"):
+    submission = pd.DataFrame({'id': ids, 'label': decoded_preds})
+    submission.to_csv(output_csv, index=False)
+    print(f"Saved submission to {output_csv}")
--- challenge-2/src/preprocessing.py
+++ challenge-2/src/preprocessing.py
@@ -1,2 +1,39 @@
 # src/preprocessing.py
 
+# This module contains the preprocessing steps for the soil image dataset.
+import os
+import pandas as pd
+from torchvision import transforms
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+
+# Dataset class for loading soil images and their labels
+class SoilDataset(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.labels = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.labels.iloc[idx, 0])
+        image = Image.open(img_path).convert("RGB")
+        label = self.labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+def get_transforms(phase="train"):
+    if phase == "train":
+        return transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ])
+    else:
+        return transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+        ])
